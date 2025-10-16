f8d3398573e7a0804bb13f30a02aaadace1e344c
vinay bhaskar <vinay bhaskar>
2025-10-16 22:59:20 +0530

    updated

--- modules/NSG/output.tf
+++ modules/NSG/output.tf
@@ -1,11 +1,3 @@
 output "nsg_id" {
     value = azurerm_network_security_group.nsg.id
 }
-
-output "nsg_id" {
-    value = azurerm_network_security_group.nsg.id
-}
-
-output "nsg_subnet" {
-    value = azurerm_subnet_network_security_group_association.subnetnsg.id
-}
