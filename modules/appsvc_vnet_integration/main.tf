d95490c306342f69e4b07d5c330c7dcfaa01ea6c
vinay bhaskar <vinay bhaskar>
2025-10-17 07:11:44 +0530

    updated

--- modules/appsvc_vnet_integration/main.tf
+++ modules/appsvc_vnet_integration/main.tf
@@ -2,3 +2,14 @@ resource "azurerm_app_service_virtual_network_swift_connection" "appsvc_vnet" {
   app_service_id = var.app_service_id
   subnet_id      = var.subnet_id
 }
+
+resource "azurerm_app_service_slot_virtual_network_swift_connection" "appsvc_slot_vnet" {
+  slot_name      = var.app_service_slot_name
+  app_service_id = var.app_service_id
+  subnet_id      = var.subnet_id
+}
+
+
+
+#subnet delegation
+Microsoft.Web/serverFarms
