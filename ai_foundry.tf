deb29654c3c0f93faa6c3d9d4311ab64e1344c7f
vinay bhaskar <vinay bhaskar>
2025-11-12 22:55:18 +0530

    updated Prv Endpoint

--- ai_foundry.tf
+++ ai_foundry.tf
@@ -44,6 +44,48 @@ resource "azurerm_ai_services" "example" {
   sku_name            = "S0"
 }
 
+# ----------------------------
+# Private Endpoint for AI Services
+# ----------------------------
+resource "azurerm_private_endpoint" "ai_pe" {
+  name                = "pe-ai-foundry"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  subnet_id           = azurerm_subnet.privateendpoint_snet.id
+
+  private_service_connection {
+    name                           = "aifoundry-privateserviceconnection"
+    private_connection_resource_id  = azurerm_ai_services.ai_foundry.id
+    subresource_names               = ["account"] # required for cognitive services / AI services
+    is_manual_connection            = false
+  }
+}
+
+# ----------------------------
+# Private DNS Zone Integration
+# ----------------------------
+resource "azurerm_private_dns_zone" "dns_zone" {
+  name                = "privatelink.cognitiveservices.azure.com"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_vnet_link" {
+  name                  = "dns-link-ai"
+  resource_group_name   = azurerm_resource_group.rg.name
+  private_dns_zone_name = azurerm_private_dns_zone.dns_zone.name
+  virtual_network_id    = azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_private_dns_a_record" "ai_dns_record" {
+  name                = azurerm_ai_services.ai_foundry.custom_subdomain_name
+  zone_name           = azurerm_private_dns_zone.dns_zone.name
+  resource_group_name = azurerm_resource_group.rg.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.ai_pe.private_service_connection[0].private_ip_address]
+}
+
+
+
 resource "azurerm_ai_foundry" "example" {
   name                = "exampleaihub"
   location            = azurerm_ai_services.example.location
